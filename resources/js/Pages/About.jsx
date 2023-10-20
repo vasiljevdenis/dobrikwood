c2b8e6f8e963a798c843fc4125fb542bf14d7627
Denis <vasiljevdenis@bk.ru>
2023-10-20 17:29:09 +0300

    ed

--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -42,54 +42,8 @@ const About = () => {
           width: '100%'
         }} />
       </Box>
-      <Typography variant="body1" component="p" mx={2} gutterBottom>
-      {state.text}
-      </Typography>
-      <Typography variant="body1" component="p" mx={2} gutterBottom>
-        В городе на Волге - Чебоксарах есть <b>семейная мастерская "Добрик-wood"</b>, где делают <b>изделия из дерева с любовью.</b>
-      </Typography>
-      <Typography variant="body1" component="p" mx={2} gutterBottom>
-        <b>В 2017 году</b> была создана первая лошадка-качалка для дочери, с неё и <b>началась история нашей мастерской.</b>
-      </Typography>
-      <Typography variant="body1" component="p" mx={2} gutterBottom>
-        Начиналось всё как "хобби после работы" в самом обычном гараже.
-      </Typography>
-      <Typography variant="body1" component="p" mx={2} gutterBottom>
-        Но, когда мы увидели какой отклик и какая поддержка идёт от людей - наших клиентов, мы поняли что то, что мы делаем <b>нужно людям</b>, и это вдохновило нас на дальнейший рост и развитие.
-      </Typography>
-      <Typography variant="body1" component="p" mx={2} gutterBottom>
-        Сейчас у нас есть полноценная мастерская по дереву, мы активно расширяем ассортимент нашей продукции.
-      </Typography>
-      <Typography variant="body1" component="p" mx={2} gutterBottom>
-        Для изготовления мебели, лошадок-качалок мы стараемся выбирать только <b>безопасные, экологичные материалы</b>, такие как: бук, дуб, берёза, березовая фанера высшего сорта.
-      </Typography>
-      <Typography variant="body1" component="p" mx={2} gutterBottom>
-        Как правило, такие изделия <b>не найти в обычном магазине.</b>
-      </Typography>
-      <Typography variant="body1" component="p" mx={2} gutterBottom>
-        Над каждым заказом работаем индивидуально, стараемся учесть все пожелания клиента.
-      </Typography>
-      <Typography variant="body1" component="p" mx={2} gutterBottom>
-        Так как мы сами воспитываем двоих детей, мы хорошо понимаем, что <b>важно каждому заботливому родителю.</b>
-      </Typography>
-      <Typography variant="body1" component="p" mx={2} gutterBottom>
-        В первую очередь, всё то что предназначено для детей должно быть <b>безопасным.</b>
-      </Typography>
-      <Typography variant="body1" component="p" mx={2} gutterBottom>
-        Все контактные места деталей <b>сглаженные</b> - нет острых углов, об которые ребенок может больно удариться.
-      </Typography>
-      <Typography variant="body1" component="p" mx={2} gutterBottom>
-        <b>Тщательная шлифовка</b>, чтобы не было шершавых краёв и зазубрин.
-      </Typography>
-      <Typography variant="body1" component="p" mx={2} gutterBottom>
-        Лако-красочные материалы на водной основе, без запаха.
-      </Typography>
-      <Typography variant="body1" component="p" mx={2} gutterBottom>
-        Стараемся делать на совесть, при аккуратном использовании наши изделия будут служить долго, не только для одного ребёнка.
-      </Typography>
-      <Typography variant="body1" component="p" mx={2} gutterBottom>
-        Так же мы считаем что мебель для детей должна быть эстетичной, стильной, чтобы в детской комнате было уютно и красиво, а так же чтобы с детства привить ребенку хороший вкус и чувство стиля.
-      </Typography>
+      <Typography variant="body1" component="p" mx={2} gutterBottom dangerouslySetInnerHTML={{ __html: state.text }}>
+      </Typography>      
     </>
   )
 };
--- resources/js/Pages/Delivery.jsx
+++ resources/js/Pages/Delivery.jsx
@@ -1,88 +1,46 @@
 import { Box, Typography } from "@mui/material";
 import * as React from "react";
 import dostavka from '../../images/dostavka.jpg';
+import axios from "axios";
 
 const Delivery = () => {
+
+    const [state, setState] = React.useState({
+        id: 1,
+        image: "",
+        page: "delivery",
+        text: "",
+        title: ""
+      });
+    
+      React.useEffect(() => {
+        axios.get(import.meta.env.VITE_APP_BASE_URL + '/api/page/delivery')
+          .then(res => {
+            console.log(res);
+            let json = res.data[0];
+            setState((oldState) => ({ ...oldState, title: json.title, text: json.text, image: json.image }));
+          })
+          .catch(err => {
+          })
+          .finally(() => {
+          });
+      }, []);
+
     return (
         <>
             <Typography variant="h4" component="h2" m={2}>
-                Условия доставки
+            {state.title}
             </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                Доставка по России осуществляется <b>Почтой России</b> или транспортной компанией <b>СДЭК</b> на выбор
-                заказчика.
-                Возможна доставка курьером до двери.
-                Также, по желанию клиента, отправляем транспортными компаниями: DPD, Boxberry, ПЭК и другими.
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                Лошадки-качалки, мебель отправляются В РАЗОБРАННОМ ВИДЕ в комплекте с инструкцией по сборке и крепежом. Сроки и стоимость доставки рассчитываются индивидуально, т.к. зависит от местоположения города.
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                <b>Самовывоз</b> доступен в городе Чебоксары.
+            <Typography variant="body1" component="p" mx={2} gutterBottom dangerouslySetInnerHTML={{ __html: state.text.slice(0, state.text.indexOf('<h2>')) }}>                
             </Typography>
             <Box sx={{ width: '100%', textAlign: 'center' }}>
-                <img src={dostavka} alt="О нас" style={{
+                <img src={state.image} alt={state.title} style={{
                     maxWidth: '500px',
                     width: '100%',
                     margin: 1
                 }} />
             </Box>
-            <Typography variant="h4" component="h2" m={2}>
-                Условия оплаты
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                Для товаров в наличии заказ принимается по предоплате 100%.
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                Отправка осуществляется в течении 1-2 рабочих дней (пн-пт) с момента оплаты.
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                Если изделие изготавливается по предзаказу, то 50 % стоимости оплачивается при оформлении заказа.
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                С момента внесения оплаты клиентом заказ считается принятым. Остальная часть оплачивается по готовности, мы высылаем заказчику фото готового изделия.
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                Срок изготовления занимает ориентировочно 5-7 дней (лучше уточнить дополнительно при оформлении заказа). Отправляем посылку по факту полной оплаты клиентом.
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                Оплата за доставку: Почтой - в день отправки посылки, СДЭКом – при получении посылки. Отправка осуществляется из города Чебоксары.
-            </Typography>
-            <Typography variant="h5" component="h3" m={2}>
-                Возврат товара
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                Что делать, если Вы получили бракованный товар? Тут следует сразу оговорить, что считается браком:
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                Брак – это значительное повреждение/дефект явно заметный глазу, делающий невозможной дальнейшую эксплуатацию изделия.
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                Если сразу после получения и осмотра товара, вы обнаружите такой дефект – свяжитесь с нами через форму для обратной связи или WhatsApp 89196628330, прислав фотографию, на которой отчетливо виден брак.
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                Если факт недоброкачественности товара действительно подтвердится, возможен обмен товара или полный возврат его стоимости – на выбор, а также возмещение всех расходов, связанных с его как прямой, так и обратной доставкой – в течении 7 дней после возврата.
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                ВНИМАНИЕ! Браком не могут являться мелкие эстетические несовершенства, естественные особенности строения древесины, такие как сучки, смоляные карманы, микротрещины и мелкие сколы.
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                Если дефект малозначительный, не влияющий на эксплуатационные характеристики – не зависимо от того, при каких условиях произошло повреждение изделия – возможна выплата денежной компенсации.
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                Размер компенсации устанавливается индивидуально, в зависимости от степени повреждения изделия.
-            </Typography>
-            <Typography variant="h5" component="h3" m={2}>
-                Возврат качественного товара
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                Если наше изделие не устроило вас по одной из причин — не подошел размер, не устроил цвет, дизайн или Вы просто хотите отменить покупку, Вы можете в течение 7 дней после получения посылки возвратить товар любым удобным для Вас способом.
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                При этом, изделие не должно иметь видимых следов эксплуатации, сохранять первоначальный товарный вид, а так же должно быть надёжно упаковано для защиты от повреждений при транспортировке.
-            </Typography>
-            <Typography variant="body1" component="p" mx={2} gutterBottom>
-                При соблюдении этого условия - мы вернем деньги в течение 3-х рабочих дней после его возврата. Транспортные расходы в этом случае оплачивает покупатель.
+            <Typography variant="body1" component="p" mx={2} gutterBottom dangerouslySetInnerHTML={{ __html: state.text.slice(state.text.indexOf('<h2>')) }}>
             </Typography>
         </>
     )
